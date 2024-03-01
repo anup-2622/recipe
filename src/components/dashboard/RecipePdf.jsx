303a35d787b6241bc09e9ccc832352c98b10ed0e
Anup <anupisri107@gmail.com>
2024-03-01 23:32:32 +0530

    working on PDF Template for users

--- src/components/dashboard/RecipePdf.jsx
+++ src/components/dashboard/RecipePdf.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
+
 const RecipePdf = (props) => {
+    console.log(props)
   return (
-    <div></div>
+    <>
+    <Document>
+        <Page>
+            <View>
+
+            <Text >{props.recipeDetails.recipeName}</Text>
+            </View>
+        </Page>
+    </Document>
+    </>
   )
 }
 
